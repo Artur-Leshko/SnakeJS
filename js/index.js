fac2c592beb1ff41733ecd516427258970dc7c6d
Artur Leshko <fire.ghost.01@mail.ru>
2021-04-05 22:25:44 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const clientWidth = document.documentElement.clientWidth;
+const clientHeight = document.documentElement.clientHeight;
+
+const canvas = document.querySelector('#myCanvas');
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
