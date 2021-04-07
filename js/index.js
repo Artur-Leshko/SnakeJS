0c4601c3853f345469cd5a90605b2ecb02084fdd
Artur Leshko <fire.ghost.01@mail.ru>
2021-04-07 23:50:45 +0300

    add start html/scc/js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,35 @@
-const clientWidth = document.documentElement.clientWidth;
-const clientHeight = document.documentElement.clientHeight;
+const
+    clientWidth = document.documentElement.clientWidth,
+    clientHeight = document.documentElement.clientHeight,
+    startBtn = document.querySelector('.start-btn'),
+    exitBtn = document.querySelector('.exit-btn'),
+    menu = document.querySelector('.menu'),
+    canvas = document.querySelector('#myCanvas'),
+    ctx = canvas.getContext("2d");
 
-const canvas = document.querySelector('#myCanvas');
-const ctx = canvas.getContext("2d");
+let
+    fps = 1000/60,
+    timerId;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = clientWidth - document.querySelector('.score').width;
+canvas.height = clientHeight - document.querySelector('.score').height;
 
+
+
+function loop() {
+    console.log('Hello');
+}
+
+
+
+startBtn.addEventListener('click', (e) => {
+
+});
+
+exitBtn.addEventListener('click', (e) => {
+    window.close();
+});
+
+
+
+//timerId = setInterval(loop, fps);
